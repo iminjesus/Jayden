ed267a69bd7cc0b7084cccfabdbab1ce4b6cb9e8
iminjesus <43739770+iminjesus@users.noreply.github.com>
2020-05-24 18:19:47 +1000

    Update Order.cs
    Delete unused method

--- 1D array/Order.cs
+++ 1D array/Order.cs
@@ -1,4 +1,12 @@
-﻿using System;
+//*********************************************************************
+// Programmer: Jusuk Bang
+// Date: 24 May 2020
+// Software: Microsoft Visual Studio 2019 Version 16.4.5
+// Platform: Microsoft Windows 10 Professional 64‐bit 
+// Purpose:Develop Nested Array and implement a comparator
+//*********************************************************************
+
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -9,26 +17,18 @@ namespace _1D_array
 {
     class Order
     {
-        public List<OrderLine> _orderLines = new List<OrderLine>();
-        //private int nextID = 1;
-        //private double DiscountMultiplier = 0.85;
+        public List<OrderLine> _orderLines = new List<OrderLine>();//create orderLine array
 
         public Order()
         { 
         }
-        public List<OrderLine> GetList()
-        {
-            return _orderLines;
-        }
 
-
-        public void AddOrderLine(OrderLine line)
+        public void AddOrderLine(OrderLine line) //Add new Item
         {
             _orderLines.Add(line);
-
         }
 
-        public double OrderTotal()
+        public double OrderTotal()//Calculate all order price by adding each line order amount
         {
             double total = 0;
             foreach (OrderLine line in _orderLines)
@@ -41,31 +41,19 @@ namespace _1D_array
         // Nested class
         internal class OrderLine : IComparable<OrderLine>
         {
-            public string ProductName { get; set; }
+            public string ProductName { get; set; }//Define valiables of Orderline class
             public int Quantity { get; set; }
             public double Price { get; set; }
 
-            public double OrderLineTotal(Order order)
+            public double OrderLineTotal(Order order)//calculate each line order amount
             {
                 return Math.Round(Price * Quantity);
             }
-            public int CompareTo(OrderLine other)
+            public int CompareTo(OrderLine other) // Alphabetic sort by using comparator
             {
-                // Alphabetic sort if salary is equal. [A to Z]
-               /* if (this.ProductName == other.ProductName)
-                {
-                    return this.ProductName.CompareTo(other.ProductName);
-                }
-                // Default to salary sort. [High to low]
-                return other.ProductName.CompareTo(this.ProductName);*/
                 return String.Compare(this.ProductName, other.ProductName);
             }
-
-            /*public override string ToString()
-            {
-                // String representation.
-                return this.ProductName.ToString() + "," + this.ProductName;
-            }*/
         }
     }
 }
+
