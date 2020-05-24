3ca8ce46421d387ba1abd45a7eab1425db468feb
iminjesus <43739770+iminjesus@users.noreply.github.com>
2020-05-24 17:53:01 +1000

    Update main.cs

--- 1D array/main.cs
+++ 1D array/main.cs
@@ -1,67 +1,63 @@
-﻿//*********************************************************************
+//*********************************************************************
 // Programmer: Jusuk Bang
-// Date: 05 May 2020
+// Date: 24 May 2020
 // Software: Microsoft Visual Studio 2019 Version 16.4.5
 // Platform: Microsoft Windows 10 Professional 64‐bit 
-// Purpose:Develop 1D array with different data type
+// Purpose:Develop Nested Array and implement a comparator
 //*********************************************************************
+
 using System;
 using System.Collections.Generic;
-using System.ComponentModel;
-using System.Data;
-using System.Drawing;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-using System.Windows.Forms;
 
 
 namespace _1D_array
 {
-    public partial class FrmMain : Form
+    class Order
     {
-        Order order = new Order();
-        public FrmMain()
-        {
-            InitializeComponent();
+        public List<OrderLine> _orderLines = new List<OrderLine>();//create orderLine array
+
+        public Order()
+        { 
         }
-        private void FrmMain_Load(object sender, EventArgs e)
+        public List<OrderLine> GetList() //Get product details
         {
-           
-        }
-
-        private void btnAdd_Click(object sender, EventArgs e)
-        {
-           
-            List<string> _orderLines = new List<string>();
-
-            //Order.OrderLine line1 = new Order.OrderLine();
-            Order.OrderLine newItem = new Order.OrderLine();
-            newItem.ProductName = txtName.Text;
-            newItem.Quantity = int.Parse(txtQuantity.Text);
-            newItem.Price = double.Parse(txtPrice.Text);
-            order.AddOrderLine(newItem);
-            txtTotal.Text = newItem.OrderLineTotal(order).ToString();
-          
-
+            return _orderLines;
         }
 
-        private void btnTotal_Click(object sender, EventArgs e)
+
+        public void AddOrderLine(OrderLine line) //Add new Item
         {
-            txtDisplay.Text = order.OrderTotal().ToString();
+            _orderLines.Add(line);
         }
 
-        private void btnSort_Click(object sender, EventArgs e)
+        public double OrderTotal()//Calculate all order price by adding each line order amount
         {
-            StringBuilder sb = new StringBuilder();
-            List<string> _orderLines = new List<string>();
-            order._orderLines.Sort();
-                foreach(var i in order._orderLines)
-                { 
-                sb.AppendFormat(i.ProductName +"\t"+ i.Price + "\t" + i.Quantity + "\r\n"); 
-                }
-            txtSort.Text = sb.ToString();
+            double total = 0;
+            foreach (OrderLine line in _orderLines)
+            {
+                total += line.OrderLineTotal(this);
+            }
+            return total;
+        }
+
+        // Nested class
+        internal class OrderLine : IComparable<OrderLine>
+        {
+            public string ProductName { get; set; }//Define valiables of Orderline class
+            public int Quantity { get; set; }
+            public double Price { get; set; }
+
+            public double OrderLineTotal(Order order)//calculate each line order amount
+            {
+                return Math.Round(Price * Quantity);
+            }
+            public int CompareTo(OrderLine other) // Alphabetic sort by using comparator
+            {
+                return String.Compare(this.ProductName, other.ProductName);
+            }
         }
     }
-    
 }
